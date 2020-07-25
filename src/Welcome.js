4df21bcfe68cde5000d60e2a16e2d20e0a96a5fd
Varma Bhupatiraju <varmab@gmail.com>
2020-07-25 10:53:54 +0530

    Initial version

---
+++ src/Welcome.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class Welcome extends Component{
+    constructor(){
+        super();
+
+        this.state={
+            clicks:0
+        }
+    }
+
+    updateClicks=()=>{
+        this.setState({
+            clicks:this.state.clicks+1
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1 onClick={this.updateClicks}>Welcome to {this.props.name} - ({this.state.clicks})</h1>
+                <p>Thank you</p>
+            </div>
+        )
+    }
+}
+
+export default Welcome;
